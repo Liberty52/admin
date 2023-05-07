808270ba891abc9cc6714a8ea12c558983ee128c
ansm6358 <ansm6358@naver.com>
2023-05-07 18:28:34 +0900

    role 검사 로직 추가

--- src/main/java/com/liberty52/admin/service/applicationservice/ReviewRemoveServiceImpl.java
+++ src/main/java/com/liberty52/admin/service/applicationservice/ReviewRemoveServiceImpl.java
@@ -1,9 +1,12 @@
 package com.liberty52.admin.service.applicationservice;
 
 import com.liberty52.admin.global.adapter.feign.AuthServiceClient;
+import com.liberty52.admin.global.exception.external.forbidden.InvalidRoleException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.liberty52.admin.global.constants.RoleConstants.ADMIN;
 
 @Transactional
 @RequiredArgsConstructor
@@ -14,6 +17,9 @@ public class ReviewRemoveServiceImpl implements ReviewRemoveService {
 
     @Override
     public void removeCustomerReview(String adminId, String role, String reviewId) {
+        if(!ADMIN.equals(role)) {
+            throw new InvalidRoleException(role);
+        }
         authServiceClient.removeCustomerReview(adminId, role, reviewId);
     }
 }
