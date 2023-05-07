2f38deba01b03603d9fab4062b95e4d064b12aa5
jinlee0 <wls4370@gmail.com>
2023-05-07 12:35:29 +0900

    feat: admin: 리뷰 답변 수정

--- src/main/java/com/liberty52/admin/service/controller/ReplyModifyController.java
+++ src/main/java/com/liberty52/admin/service/controller/ReplyModifyController.java
@@ -1,27 +1,31 @@
-package com.liberty52.admin.global.adapter.feign;
+package com.liberty52.admin.service.controller;
 
+import com.liberty52.admin.service.applicationservice.ReplyCreateService;
+import com.liberty52.admin.service.applicationservice.ReplyModifyService;
 import com.liberty52.admin.service.controller.dto.ReplyCreateRequestDto;
 import com.liberty52.admin.service.controller.dto.ReplyModifyRequestDto;
-import org.springframework.cloud.openfeign.FeignClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(value = "product", primary = false)
-public interface ProductServiceClient {
+@RequiredArgsConstructor
+@RestController
+public class ReplyModifyController {
 
+    private final ReplyModifyService replyModifyService;
 
-    @PostMapping("/reviews/{reviewId}/replies")
-    void replyCreate(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
-            @RequestHeader("X-ROLE") String role,
-            @Validated @RequestBody ReplyCreateRequestDto dto, @PathVariable String reviewId) ;
 
     @PutMapping("/reviews/{reviewId}/replies/{replyId}")
     @ResponseStatus(HttpStatus.CREATED)
-    void replyModify(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
+    public void replyModify(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
                             @RequestHeader("LB-Role") String role,
                             @Validated @RequestBody ReplyModifyRequestDto dto,
                             @PathVariable String reviewId,
-                            @PathVariable String replyId);
+                            @PathVariable String replyId) {
+        replyModifyService.modify(adminId, role, dto, reviewId, replyId);
+    }
+
 }
