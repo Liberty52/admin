45eb26cb029a2dbe736b73085a03acc0e53396ee
JIN-YONG LEE <74762475+jinlee0@users.noreply.github.com>
2023-05-12 19:27:28 +0900

    LIB-221 관리자 공지사항 수정 (#21)
    * LIB-221 관리자 공지사항 수정
    
    * check role in service
    
    * log in controller

---
+++ src/main/java/com/liberty52/admin/service/applicationservice/NoticeModifyServiceImpl.java
@@ -0,0 +1,19 @@
+package com.liberty52.admin.service.applicationservice;
+
+import com.liberty52.admin.global.adapter.feign.AuthServiceClient;
+import com.liberty52.admin.global.utils.AdminRoleUtils;
+import com.liberty52.admin.service.controller.dto.NoticeModifyRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeModifyServiceImpl implements NoticeModifyService {
+    private final AuthServiceClient authServiceClient;
+
+    @Override
+    public void modify(String role, String noticeId, NoticeModifyRequestDto dto) {
+        AdminRoleUtils.checkRole(role);
+        authServiceClient.noticeModify(role, noticeId, dto);
+    }
+}
