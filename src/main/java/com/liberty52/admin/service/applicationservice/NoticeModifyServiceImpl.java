22fe4ce460709cd4eaa86336a67596e38c53932c
jinlee0 <wls4370@gmail.com>
2023-05-12 19:23:21 +0900

    check role in service

--- src/main/java/com/liberty52/admin/service/applicationservice/NoticeModifyServiceImpl.java
+++ src/main/java/com/liberty52/admin/service/applicationservice/NoticeModifyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.liberty52.admin.service.applicationservice;
 
 import com.liberty52.admin.global.adapter.feign.AuthServiceClient;
+import com.liberty52.admin.global.utils.AdminRoleUtils;
 import com.liberty52.admin.service.controller.dto.NoticeModifyRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ public class NoticeModifyServiceImpl implements NoticeModifyService {
 
     @Override
     public void modify(String role, String noticeId, NoticeModifyRequestDto dto) {
+        AdminRoleUtils.checkRole(role);
         authServiceClient.noticeModify(role, noticeId, dto);
     }
 }
