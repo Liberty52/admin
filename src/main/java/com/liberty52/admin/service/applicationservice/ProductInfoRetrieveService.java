9f53c891b9987397cbff8cc09b6f32e65ee146d1
ansm6358 <ansm6358@naver.com>
2023-05-20 01:39:41 +0900

    상품 리스트 조회

--- src/main/java/com/liberty52/admin/service/applicationservice/ProductInfoRetrieveService.java
+++ src/main/java/com/liberty52/admin/service/applicationservice/ProductInfoRetrieveService.java
@@ -1,2 +1,11 @@
-package com.liberty52.admin.service.applicationservice;public interface ProductInfoRetrieveService {
+package com.liberty52.admin.service.applicationservice;
+
+import com.liberty52.admin.service.controller.dto.ProductInfoRetrieveResponseDto;
+
+import java.util.List;
+
+public interface ProductInfoRetrieveService {
+    List<ProductInfoRetrieveResponseDto> retrieveProductInfoList(String role);
+
+    ProductInfoRetrieveResponseDto retrieveProductInfo(String role, String productId);
 }
--- src/main/java/com/liberty52/admin/service/controller/ProductInfoRetrieveController.java
+++ src/main/java/com/liberty52/admin/service/controller/ProductInfoRetrieveController.java
@@ -1,2 +1,27 @@
-package com.liberty52.admin.service.controller;public class ProductInfoRetrieveController {
+package com.liberty52.admin.service.controller;
+
+import com.liberty52.admin.service.applicationservice.ProductInfoRetrieveService;
+import com.liberty52.admin.service.controller.dto.ProductInfoRetrieveResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductInfoRetrieveController {
+    private final ProductInfoRetrieveService productInfoRetrieveService;
+
+    @GetMapping("/productInfo")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductInfoRetrieveResponseDto> retrieveProductInfoList(@RequestHeader("LB-Role") String role) {
+        return productInfoRetrieveService.retrieveProductInfoList(role);
+    }
+
+    @GetMapping("/productInfo/{productId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductInfoRetrieveResponseDto retrieveProductInfo(@RequestHeader("LB-Role") String role, @PathVariable String productId) {
+        return productInfoRetrieveService.retrieveProductInfo(role, productId);
+    }
 }
