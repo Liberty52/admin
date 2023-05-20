984184495f9e6c08d449723551c864a6e8bce484
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-05-21 01:55:30 +0900

    관리자 리뷰 전체 조회 response 수정 (#48)
    * feat: 관리자 주문 상태 변경 API
    
    * fix: 관리자 리뷰 이슈 수정

---
+++ src/main/java/com/liberty52/admin/service/controller/OrderStatusModifyController.java
@@ -0,0 +1,37 @@
+package com.liberty52.admin.service.controller;
+
+import com.liberty52.admin.global.adapter.feign.dto.AdminVBankStatusModifyDto;
+import com.liberty52.admin.service.applicationservice.OrderStatusModifyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderStatusModifyController {
+
+  private final OrderStatusModifyService orderStatusModifyService;
+  @PatchMapping("/orders/{orderId}/status")
+  @ResponseStatus(HttpStatus.OK)
+  public void modifyOrderStatus(
+      @RequestHeader("LB-Role") String role, @PathVariable String orderId,
+      @RequestParam String orderStatus) {
+    orderStatusModifyService.modifyOrderStatus(role,orderId,orderStatus);
+  }
+
+  @PatchMapping("/orders/{orderId}/vbank")
+  @ResponseStatus(HttpStatus.OK)
+  public void modifyOrderStatusOfVBank(
+      @RequestHeader("LB-Role") String role, @PathVariable String orderId,
+      @Validated @RequestBody AdminVBankStatusModifyDto dto) {
+    orderStatusModifyService.modifyOrderStatusOfVBank(role,orderId,dto);
+  }
+
+}
