33fa071039aaa1eb576c7e04525762fe6f0abe6b
hseungho <hsh47607@naver.com>
2023-05-20 17:26:03 +0900

    chore: dto validate 수정

--- src/main/java/com/liberty52/admin/global/adapter/feign/dto/AdminOrderRefundDto.java
+++ src/main/java/com/liberty52/admin/global/adapter/feign/dto/AdminOrderRefundDto.java
@@ -1,6 +1,7 @@
 package com.liberty52.admin.global.adapter.feign.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,7 +14,7 @@ public class AdminOrderRefundDto {
     public static class Request {
         @NotBlank
         private String orderId;
-        @NotBlank
+        @NotNull
         private Integer fee;
         public static AdminOrderRefundDto.Request of(String orderId, Integer fee) {
             return new AdminOrderRefundDto.Request(orderId, fee);
