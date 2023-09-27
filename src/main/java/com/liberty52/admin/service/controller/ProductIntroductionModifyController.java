0a4556c89e89bf63918dd61401dc3f83b11fe861
Geonwoo Kim <96376539+DryRains@users.noreply.github.com>
2023-09-27 16:22:33 +0900

    Update ProductIntroductionModifyController.java
    오타 수정

--- src/main/java/com/liberty52/admin/service/controller/ProductIntroductionModifyController.java
+++ src/main/java/com/liberty52/admin/service/controller/ProductIntroductionModifyController.java
@@ -6,17 +6,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@RestController
+@RequiredArgsConstructor
 public class ProductIntroductionModifyController {
-    @RestController
-    @RequiredArgsConstructor
-    public class ProductIntroductionCreateController {
-        private final ProductIntroductionModifyService productIntroductionModifyService;
 
-        @PatchMapping("/product/{productId}/introduction")
-        @ResponseStatus(HttpStatus.NO_CONTENT)
-        public void modifyProductIntroductionByAdmin(@RequestHeader("LB-Role") String role, @PathVariable String productId,
-                                                     @RequestPart(value = "images",required = false) MultipartFile productIntroductionImageFile) {
-            productIntroductionModifyService.modifyProductIntroduction(role, productId, productIntroductionImageFile);
-        }
+    
+    private final ProductIntroductionModifyService productIntroductionModifyService;
+
+    @PatchMapping("/product/{productId}/introduction")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void modifyProductIntroductionByAdmin(@RequestHeader("LB-Role") String role, @PathVariable String productId,
+                                                 @RequestPart(value = "images",required = false) MultipartFile productIntroductionImageFile) {
+        productIntroductionModifyService.modifyProductIntroduction(role, productId, productIntroductionImageFile);
     }
 }
