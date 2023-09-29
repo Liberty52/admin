6b5ce5c0d6e05a7954704194c568fc8e0e8b1d76
DryRains <dryrain_@naver.com>
2023-09-29 19:48:39 +0900

    상품 옵션 선택지 재고량 수정

--- src/main/java/com/liberty52/admin/service/controller/dto/CreateOptionDetailRequestDto.java
+++ src/main/java/com/liberty52/admin/service/controller/dto/CreateOptionDetailRequestDto.java
@@ -1,5 +1,6 @@
 package com.liberty52.admin.service.controller.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -11,8 +12,10 @@ public class CreateOptionDetailRequestDto {
     String name;
 
     @NotNull
+    @Min(0)
     Integer price;
 
     @NotNull
     Boolean onSale;
+
 }
